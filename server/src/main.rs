a7d19ed3d7bc4a3926e76e87186312b22284eaf4
Will Beddow <will@willbeddow.com>
2024-02-11 14:51:21 -0500

    init axum server

---
+++ server/src/main.rs
@@ -0,0 +1,13 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "wip" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
