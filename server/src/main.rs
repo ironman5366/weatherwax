cb3f19dbca23c09bce437b98a90e302b44b18bc7
Will Beddow <will@willbeddow.com>
2024-03-05 15:13:39 -0500

    basic setup with axum and sse

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,33 @@
+mod types;
+
 use axum::{
     routing::get,
     Router,
+    response::sse::{Event, Sse}
 };
+use std::{convert::{Infallible}};
+use std::time::Duration;
+use tokio_stream::StreamExt as _;
+
+use futures::stream::{self, Stream};
+
+async fn invoke(
+) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    let stream = stream::iter(vec![
+        Ok(Event::default().data("hello")),
+        Ok(Event::default().data("world")),
+    ]).throttle(Duration::from_secs(3));
+
+    Sse::new(stream).keep_alive(
+        axum::response::sse::KeepAlive::new()
+            .interval(Duration::from_secs(1))
+    )
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "wip" }));
+    let app = Router::new().route("/invoke", get(invoke));
 
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
